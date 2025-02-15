b14bb7249ecf88eac2f95b196aa4d52f549b2a05
Deus <syahidazharazizi@gmail.com>
2025-02-15 10:34:22 +0700

    logging and exception

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+this is for the land fcurioser 
+whatebver im gong is pow
+wojdj ams
+delattr ModuleNotFoundErrorw ModuleNotFoundErrorw orowa matchw
+KeyboardInterrupt
+
+
+
+kokok oradn and asdn
+ nas
+  case nasi 
+  elif nei
+   eliffnf next efne 
+   FileNotFoundError FutureWarningnf aiter NotImplemented and 
+    aindloinoianwoinoin
